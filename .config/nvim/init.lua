baf01315e19e95550e712e63ec8d4ccb5a05432a
Marcopist <marcopist@example.com>
2026-06-03 09:24:15 +0100

    Neovim config update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.python3_host_prog = vim.fn.exepath("python3")
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4    -- Size of an indent
@@ -77,6 +80,90 @@ require("lazy").setup({
     "sphamba/smear-cursor.nvim",
     opts = {},
   },
+  {
+    "3rd/image.nvim",
+    build = false,
+    opts = {
+      backend = "kitty",
+      integrations = {},
+      max_width = 100,
+      max_height = 12,
+      max_height_window_percentage = math.huge,
+      max_width_window_percentage = math.huge,
+      window_overlap_clear_enabled = true,
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    },
+  },
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0",
+    build = ":UpdateRemotePlugins",
+    dependencies = { "3rd/image.nvim" },
+    init = function()
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_wrap_output = true
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
+    end,
+    keys = {
+      { "<leader>mi", "<cmd>MoltenInit<cr>",              desc = "Molten: init kernel" },
+      { "<leader>ml", "<cmd>MoltenEvaluateLine<cr>",      desc = "Molten: run line" },
+      { "<leader>mr", "<cmd>MoltenReevaluateCell<cr>",    desc = "Molten: re-run cell" },
+      { "<leader>mv", "<cmd>MoltenEvaluateVisual<cr>",    desc = "Molten: run visual", mode = "v" },
+      { "<leader>md", "<cmd>MoltenDelete<cr>",            desc = "Molten: delete output" },
+      { "<leader>mh", "<cmd>MoltenHideOutput<cr>",        desc = "Molten: hide output" },
+      { "<leader>ms", "<cmd>MoltenShowOutput<cr>",        desc = "Molten: show output" },
+    },
+    config = function()
+      -- Cell navigation: jump between # %% markers
+      vim.keymap.set("n", "]c", function()
+        vim.fn.search("^# %%", "W")
+      end, { desc = "Next cell" })
+      vim.keymap.set("n", "[c", function()
+        vim.fn.search("^# %%", "bW")
+      end, { desc = "Prev cell" })
+
+      -- Run current cell: from current # %% to next # %% (or EOF)
+      vim.keymap.set("n", "<leader>mc", function()
+        local start = vim.fn.search("^# %%", "bcnW")
+        local finish = vim.fn.search("^# %%", "nW")
+        if start == 0 then return end
+        local from = start + 1
+        local to = finish > 0 and (finish - 1) or vim.fn.line("$")
+        if from > to then return end
+        vim.fn.MoltenEvaluateRange(from, to)
+      end, { desc = "Molten: run cell" })
+
+      vim.keymap.set("n", "<leader>ma", function()
+        local last_line = vim.fn.line("$")
+        local markers = {}
+        local lnum = 1
+        while lnum <= last_line do
+          local line = vim.fn.getline(lnum)
+          if line:match("^# %%") then
+            table.insert(markers, lnum)
+          end
+          lnum = lnum + 1
+        end
+        for i, marker in ipairs(markers) do
+          local from = marker + 1
+          local to = markers[i + 1] and (markers[i + 1] - 1) or last_line
+          if from <= to then
+            vim.fn.MoltenEvaluateRange(from, to)
+          end
+        end
+      end, { desc = "Molten: run all cells" })
+    end,
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.bo.indentexpr = ""
+  end,
 })
 
 -- Enable theme
